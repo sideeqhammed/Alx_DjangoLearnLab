93dd8feceb526c4e39638a4ad253dfe9110c4220
sideeqhammed <sideeqhammed05.com>
2025-09-11 19:06:41 +0100

    added relationship_app application

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,2 +1,6 @@
+# Checks for List of all books in a library.
 library = Library.objects.get(name=library_name)
 books = library.books.all()
+
+# Checks for all books by a specific author
+books = Book.objects.filter(author=author_name)
