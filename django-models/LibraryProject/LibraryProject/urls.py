a6f063cb3e017709717785723799f86fa16545b8
sideeqhammed <sideeqhammed05.com>
2025-09-19 10:30:46 +0100

    Updated djamgo-models

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name = 'relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name = 'relationship_app/logout.html'), name='logout')
 ]
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
   path('books/', list_books, name='books_list'),
   path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_details'),
+
 ]
