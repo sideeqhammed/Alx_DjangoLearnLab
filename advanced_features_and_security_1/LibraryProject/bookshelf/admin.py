9a67cb838444ba7ba91941205f8e1c1be33355f9
sideeqhammed <sideeqhammed05.com>
2025-09-25 17:37:14 +0100

    Added advanced_features_and_security folder

---
+++ advanced_features_and_security_1/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Book, CustomUser
+
+# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+  list_display = ["title", "author", "publication_year"]
+  list_filter = ["author", "publication_year"]
+  search_fields = ["title", "author"]
+  pass
+
+admin.site.register(Book)
+admin.site.register(CustomUser)
