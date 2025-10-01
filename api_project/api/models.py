a933d1a74a2eaaa4a4a46bb9ceea9ccc67f54687
sideeqhammed <sideeqhammed05.com>
2025-10-01 12:36:59 +0100

    Implemented CRUD operations using viewsets

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,9 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+import datetime
 
 # Create your models here.
 class Book (models.Model):
   title = models.CharField(max_length=64)
   author = models.CharField(max_length=64)
-  published_date = models.DateField(null=True, blank=True)
+  published_year = models.PositiveSmallIntegerField(
+    validators=[MinValueValidator(1450), MaxValueValidator(datetime.date.today().year)], null=True, blank=True
+  )
+
+  def __str__(self):
+    return self.title
+
 
--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -2,5 +2,6 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
-  model = Book
-  fields = ['title', 'author', 'published_date']
+  class Meta:
+    model = Book
+    fields = ['title', 'author', 'published_year']
