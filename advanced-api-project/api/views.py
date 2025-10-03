b3997e7ca4e22503f44e7e06a87da2959af461e7
sideeqhammed <sideeqhammed05.com>
2025-10-03 18:49:52 +0100

    Built Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Book
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from django.contrib.auth.mixins import LoginRequiredMixin
 # from .serializers import BookSerializer
 
 # Create your views here.
@@ -9,25 +11,30 @@ class BookListView(ListView):
   model = Book
   template_name = 'api/book_list.html'
   context_object_name = 'books'
+  permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookDetailView(DetailView):
   model = Book
   template_name = 'api/book_detail.html'
   context_object_name = 'book'
+  permission_classes = [IsAuthenticatedOrReadOnly]
 
-class BookCreateView(CreateView):
+class BookCreateView(LoginRequiredMixin, CreateView):
   model = Book
   template_name = 'api/book_form.html'
   fields = ['title','author', 'publication_year']
   success_url = reverse_lazy('book-list')
+  permission_classes = [IsAuthenticated]
 
-class BookUpdateView(UpdateView):
+class BookUpdateView(LoginRequiredMixin, UpdateView):
   model = Book
   template_name = 'api/book_form.html'
   fields = ['title', 'author', 'publication_year']
   success_url = reverse_lazy('book-list')
+  permission_classes = [IsAuthenticated]
 
-class BookDeleteView(DeleteView):
+class BookDeleteView(LoginRequiredMixin, DeleteView):
   model = Book
   template_name = 'api/book_delete.html'
   success_url = reverse_lazy('book-list')
+  permission_classes = [IsAuthenticated]
