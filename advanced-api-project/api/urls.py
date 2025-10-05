50e556e4d0c4077c1d86d8a7bc258b56407f78d1
sideeqhammed <sideeqhammed05.com>
2025-10-05 12:08:18 +0100

    Implemented Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from .views import BookListApiView, BookDetailApiView, BookCreateApiView, BookUpdateApiView, BookDeleteApiView
 
 urlpatterns = [
+  path('', BookListApiView.as_view(), name='book-api-list'),
+  path('<int:pk>/', BookDetailApiView.as_view(), name='book-api-detail'),
+  path('createapi/', BookCreateApiView.as_view(), name='create-api-book'),
+  path('updateapi/', BookUpdateApiView.as_view(), name='update-api-book'),
+  path('deleteapi/', BookDeleteApiView.as_view(), name='delete-api-book'),
+
+
+
   path('books/', BookListView.as_view(), name='book-list'),
   path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-  path('books/create', BookCreateView.as_view(), name='create-book'),
-  path('books/update', BookUpdateView.as_view(), name='update-book'),
-  path('books/delete/', BookDeleteView.as_view(), name='delete-book')
+  path('books/create/', BookCreateView.as_view(), name='create-book'),
+  path('books/update/', BookUpdateView.as_view(), name='update-book'),
+  path('books/delete/', BookDeleteView.as_view(), name='delete-book'),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,40 +1,81 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from rest_framework import generics, filters
+from .serializers import BookSerializer
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Book
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django.contrib.auth.mixins import LoginRequiredMixin
-# from .serializers import BookSerializer
+from django_filters import rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
+
+class BookListApiView(generics.ListAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
+
+  filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+  filterset_fields = ['title', 'author__name', 'publication_year']
+  search_fields = ['title', 'author__name']
+  ordering_fields = ['title', 'publication_year']
+
+class BookDetailApiView(generics.RetrieveAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookCreateApiView(generics.CreateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [IsAuthenticated]
+
+class BookUpdateApiView(generics.UpdateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [IsAuthenticated]
+
+class BookDeleteApiView(generics.DestroyAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [IsAuthenticated]
+
+
+
+
+
+
+
+
+
+
+
+
+
 class BookListView(ListView):
   model = Book
   template_name = 'api/book_list.html'
   context_object_name = 'books'
-  permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookDetailView(DetailView):
   model = Book
   template_name = 'api/book_detail.html'
   context_object_name = 'book'
-  permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookCreateView(LoginRequiredMixin, CreateView):
   model = Book
   template_name = 'api/book_form.html'
   fields = ['title','author', 'publication_year']
   success_url = reverse_lazy('book-list')
-  permission_classes = [IsAuthenticated]
 
 class BookUpdateView(LoginRequiredMixin, UpdateView):
   model = Book
   template_name = 'api/book_form.html'
   fields = ['title', 'author', 'publication_year']
   success_url = reverse_lazy('book-list')
-  permission_classes = [IsAuthenticated]
 
 class BookDeleteView(LoginRequiredMixin, DeleteView):
   model = Book
   template_name = 'api/book_delete.html'
   success_url = reverse_lazy('book-list')
-  permission_classes = [IsAuthenticated]
